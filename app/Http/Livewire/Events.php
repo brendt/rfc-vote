20283eb279106cfbdb0db55031a17f53b30dc67d
Mohammad_Alhallaq <mohammad.alhallaq@gmail.com>
2023-08-03 20:19:49 +0300

    correct the enum value

--- app/Http/Livewire/Events.php
+++ app/Http/Livewire/Events.php
@@ -5,7 +5,7 @@ namespace App\Http\Livewire;
 enum Events: string
 {
     case USER_VOTED = 'user_voted';
-    case USER_UNDO_VOTE = 'user_UNDO_VOTE';
+    case USER_UNDO_VOTE = 'user_undo_vote';
     case REPUTATION_UPDATED = 'reputation_updated';
     case ARGUMENT_CREATED = 'argument_created';
 }
