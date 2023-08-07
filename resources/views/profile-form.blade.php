05b33a6eb5dfd435d01663482d264efab6fd59cf
Brent Roose <brent.roose@gmail.com>
2023-08-07 14:28:50 +0200

    wip

--- resources/views/profile-form.blade.php
+++ resources/views/profile-form.blade.php
@@ -1,20 +1,24 @@
 @component('layouts.base')
     <div class="grid mx-auto container max-w-[800px] px-4 gap-6 mt-4 md:mt-12 mb-8">
-        <x-form class="bg-white p-4 shadow-lg border-gray-300 border"
+        <x-form class="bg-white p-4 shadow-lg border-gray-300 border grid gap-2 grid-cols-2"
                 action="{{ action([\App\Http\Controllers\ProfileController::class, 'update']) }}"
                 method="post"
                 enctype="multipart/form-data"
         >
 
-            <h2 class="text-xl font-bold">Profile</h2>
+            <h2 class="text-xl font-bold col-span-2">Profile</h2>
 
             @bind($user)
 
-            <x-form-input type="text" name="name" label="Name" />
+            <div class="col-span-2">
+                <x-form-input type="text" name="name" label="Name" />
+            </div>
 
-            <div>
-                <img src="{{ $user->getAvatarUrl() }}" />
-                {{ $user->getAvatarUrl() }}
+            <div class="flex col-span-2 gap-4 items-center mt-4">
+                @if($user->getAvatarUrl())
+                    <img src="{{ $user->getAvatarUrl() }}" class="border-4 border-purple-800 shadow-xl rounded-full max-w-[100px]"/>
+                @endif
+
                 <x-form-input type="file" name="avatar" label="Choose a new avatar" />
             </div>
 
