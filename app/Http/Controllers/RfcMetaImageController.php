b90e2116502c0bf72436214b047e1704f6077990
brendt <brendt@users.noreply.github.com>
2023-08-08 12:44:36 +0000

    Fix styling

--- app/Http/Controllers/RfcMetaImageController.php
+++ app/Http/Controllers/RfcMetaImageController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Rfc;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Storage;
 use Spatie\Browsershot\Browsershot;
 
 final readonly class RfcMetaImageController
