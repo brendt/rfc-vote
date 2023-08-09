dff5a1305bc4e133d4fc05f8c4531299fffc42b0
Brent Roose <brent.roose@gmail.com>
2023-08-09 08:34:03 +0200

    wip

---
+++ app/Console/Commands/BrowsershotCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Browsershot\Browsershot;
+
+class BrowsershotCommand extends Command
+{
+    protected $signature = 'browsershot';
+
+    public function handle(): void
+    {
+        $browsershot = Browsershot::html('<h1>hi</h1>');
+
+        if ($chromePath = config('services.chrome.path')) {
+            $browsershot->setChromePath($chromePath);
+        }
+
+        $image = $browsershot
+            ->windowSize(1200, 627)
+            ->deviceScaleFactor(2)
+            ->setScreenshotType('png')
+            ->base64Screenshot();
+
+        $this->info(strlen($image));
+    }
+}
