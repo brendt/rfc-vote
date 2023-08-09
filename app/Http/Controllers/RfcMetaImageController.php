2e3e43ead6ee9a5a9eef0c807afe1c8a2674fd1c
Brent Roose <brent.roose@gmail.com>
2023-08-09 08:43:13 +0200

    wip

--- app/Http/Controllers/RfcMetaImageController.php
+++ app/Http/Controllers/RfcMetaImageController.php
@@ -12,17 +12,26 @@ final readonly class RfcMetaImageController
 {
     public function __invoke(Rfc $rfc, Request $request)
     {
-        if ($request->has('nocache')) {
-            $image = $this->generateImage($rfc);
-        } else {
-            $image = Cache::remember(
-                key: "meta-{$rfc->id}",
-                ttl: now()->addMinutes(15),
-                callback: fn () => $this->generateImage($rfc),
-            );
-        }
+//        if ($request->has('nocache')) {
+//            $image = $this->generateImage($rfc);
+//        } else {
+//            $image = Cache::remember(
+//                key: "meta-{$rfc->id}",
+//                ttl: now()->addMinutes(15),
+//                callback: fn () => $this->generateImage($rfc),
+//            );
+//        }
+
+
+        $html = view('rfc-meta', [
+            'rfc' => Rfc::first(),
+        ])->render();
+
+        $image = (new RenderMetaImage())($html);
 
         Log::debug(strlen($image));
+
+        return $image;
 
         return response(base64_decode($image))->header('Content-Type', 'image/png');
     }
