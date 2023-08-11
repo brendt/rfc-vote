697c9303cee0c18954c23bae28065c18c2fc6989
brendt <brendt@users.noreply.github.com>
2023-08-11 11:59:21 +0000

    Fix styling

--- database/migrations/2023_08_11_133415_update_vote_counts.php
+++ database/migrations/2023_08_11_133415_update_vote_counts.php
@@ -3,7 +3,8 @@
 use App\Models\Rfc;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Rfc::each(fn (Rfc $rfc) => $rfc->updateVoteCount());
