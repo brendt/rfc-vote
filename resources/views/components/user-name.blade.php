950a1878e24fea3ef02633dae6432a6ed76f1da9
SerhiiCho <serhiicho@protonmail.com>
2023-08-12 11:49:50 +0300

    Removed border around the user avatar on argument-card

--- resources/views/components/user-name.blade.php
+++ resources/views/components/user-name.blade.php
@@ -2,9 +2,9 @@
     /** @var \App\Models\User $user */
 @endphp
 
-<span class="flex items-center gap-2 relative group">
+<span class="flex items-center gap-3 relative group">
 @if($user->getAvatarUrl())
-    <img src="{{ $user->getAvatarUrl() }}" class="border-purple-200 border-2 rounded-full w-[30px] h-[30px]"/>
+    <img src="{{ $user->getAvatarUrl() }}" class="rounded-full shadow-sm w-[30px] h-[30px]"/>
 @endif
     {{ $user->name }} <x-user-popup :user="$user"/>
 </span>
