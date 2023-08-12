74f3f68ae7d1d30e33e15973dc49259f2d6cff20
MohammadAlhallaq <MohammadAlhallaq@users.noreply.github.com>
2023-08-12 20:39:39 +0000

    Fix styling

--- app/Actions/ToggleArgumentVote.php
+++ app/Actions/ToggleArgumentVote.php
@@ -28,7 +28,7 @@ final readonly class ToggleArgumentVote
                     reputationType: ReputationType::GAIN_ARGUMENT_VOTE,
                 );
             } else {
-                if (!$argument->user()->is($user)) {
+                if (! $argument->user()->is($user)) {
                     ArgumentVote::create([
                         'argument_id' => $argument->id,
                         'user_id' => $user->id,
