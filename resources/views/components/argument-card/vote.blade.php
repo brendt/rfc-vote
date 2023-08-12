0845184731798d441a2b9985a11917c2f2c394d9
SerhiiCho <serhiicho@protonmail.com>
2023-08-12 13:39:23 +0300

    Added 'yes' and 'no' under the vote on argument card

--- resources/views/components/argument-card/vote.blade.php
+++ resources/views/components/argument-card/vote.blade.php
@@ -1,10 +1,9 @@
 <div
     class="
-            px-3
-            py-1
             relative
             cursor-pointer
             transition-colors
+            min-w-[50px]
             text-center
             rounded-full
             group
@@ -20,8 +19,17 @@
         wire:click="voteForArgument({{ $argument->id }})"
 >
     <x-icons.chevron-up
-        class="w-5 h-5 cursor-pointer text-black -mb-1 text-inherit group-hover:-translate-y-1 transition-all group-hover:stroke-[3]"
+        class="w-5 h-5 cursor-pointer text-black m-auto -mb-1 text-inherit group-hover:-translate-y-1 transition-all group-hover:stroke-[3]"
     ></x-icons.chevron-up>
 
     <span class="font-bold">{{ $argument->vote_count }}</span>
+
+    <div class="text-center">
+        @if ($argument->vote_type->getColor() === 'green')
+            <span class="text-[1rem] font-bold uppercase">Yes</span>
+        @else
+            <span class="text-[1rem] font-bold uppercase">No</span>
+        @endif
+    </div>
+
 </div>
