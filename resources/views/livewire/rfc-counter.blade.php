3c490fa2fd81b3f94cfcccc12ae376d14d5ffaed
brendt <brent.roose@gmail.com>
2023-08-13 14:29:12 +0200

    wip

--- resources/views/livewire/rfc-counter.blade.php
+++ resources/views/livewire/rfc-counter.blade.php
@@ -1,4 +1,4 @@
-<x-tag class="{{ $voteType->class(yes: 'bg-green-500', no: 'bg-red-500') }} text-white font-bold">
+<x-tag class="{{ $voteType->class(yes: 'bg-green-600', no: 'bg-red-600') }} text-white font-bold">
     {{ $count }}
     {{ $voteType->value }}
 </x-tag>
