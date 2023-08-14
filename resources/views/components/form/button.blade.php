b68a81662fa07a8994122b96514f90fe5ee98841
SerhiiCho <serhiicho@protonmail.com>
2023-08-14 18:04:18 +0300

    Changed rounded-lg to rounded-md for profile form buttons

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -5,6 +5,6 @@
      */
 @endphp
 
-<button {{ $attributes->merge(['class' => 'bg-purple-800 text-white rounded-lg px-5 py-2 mt-3 shadow-md transition-all hover:bg-purple-900 hover:shadow-lg']) }}>
+<button {{ $attributes->merge(['class' => 'bg-purple-800 text-white rounded-md px-5 py-2 mt-3 shadow-md transition-all hover:bg-purple-900 hover:shadow-lg']) }}>
     {{ $slot }}
 </button>
