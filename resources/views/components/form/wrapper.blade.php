708f0d25fad844e02e0d12e321e0b065dcbba5b6
SerhiiCho <serhiicho@protonmail.com>
2023-08-14 17:35:32 +0300

    Added margin bottom for the form wrapper title

--- resources/views/components/form/wrapper.blade.php
+++ resources/views/components/form/wrapper.blade.php
@@ -8,7 +8,7 @@
 
 <form {{ $attributes->merge(['class' => 'bg-white p-6 rounded-lg shadow-md']) }}>
     @isset($heading)
-        <h2 class="text-2xl font-bold text-gray-700">{{ $heading }}</h2>
+        <h2 class="text-2xl font-bold text-gray-700 mb-4">{{ $heading }}</h2>
     @endisset
 
     {{ $slot }}
