380afb3286a2afe2743ef7cdab0b070392fe4c7f
SerhiiCho <serhiicho@protonmail.com>
2023-08-14 17:47:57 +0300

    Applied new blade components to a profile form

--- resources/views/components/form/wrapper.blade.php
+++ resources/views/components/form/wrapper.blade.php
@@ -7,6 +7,8 @@
 @endphp
 
 <form {{ $attributes->merge(['class' => 'bg-white p-6 rounded-lg shadow-md']) }}>
+    @csrf
+
     @isset($heading)
         <h2 class="text-2xl font-bold text-gray-700 mb-4">{{ $heading }}</h2>
     @endisset
