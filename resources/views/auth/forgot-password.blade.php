a25d57031c5f4add13b1d817204939ef25c9a20a
SerhiiCho <serhiicho@protonmail.com>
2023-08-16 10:47:02 +0300

    Added information message to the form
    Here is the message: "Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one."

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -7,6 +7,10 @@
         >
             @csrf
 
+            <p class="mb-5 text-gray-500">
+                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            </p>
+
             <x-form.input
                 :label="__('Email')"
                 type="email"
