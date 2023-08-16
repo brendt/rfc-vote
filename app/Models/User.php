f1280cb1560634a6002ac53f29bebf036362d575
Brent Roose <brent.roose@gmail.com>
2023-08-16 09:55:10 +0200

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
