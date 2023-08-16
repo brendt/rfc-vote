d0bed354641ab660659e245932a456d9c83f2ed7
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-16 15:49:10 +0300

    Enhance registration tests with validation checks
    Expanded tests in RegistrationTest.php to include additional validation tests such as username length and format checks as well as uniqueness checks. This ensures the username conditions are more rigorously tested and hence improving the robustness of the registration process in the application.

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Actions\GenerateUsername;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Laravel\Fortify\Features;
 use Laravel\Jetstream\Jetstream;
 use Tests\TestCase;
@@ -15,7 +17,7 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered(): void
     {
-        if (! Features::enabled(Features::registration())) {
+        if (!Features::enabled(Features::registration())) {
             $this->markTestSkipped('Registration support is not enabled.');
 
             return;
@@ -28,7 +30,7 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register(): void
     {
-        if (! Features::enabled(Features::registration())) {
+        if (!Features::enabled(Features::registration())) {
             $this->markTestSkipped('Registration support is not enabled.');
 
             return;
@@ -37,15 +39,54 @@ class RegistrationTest extends TestCase
         $name = 'Test User';
 
         $response = $this->post('/register', [
-            'name' => $name,
-            'username' => (new GenerateUsername)($name),
-            'email' => 'test@example.com',
-            'password' => 'password',
+            'name'                  => $name,
+            'username'              => (new GenerateUsername)($name),
+            'email'                 => 'test@example.com',
+            'password'              => 'password',
             'password_confirmation' => 'password',
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+            'terms'                 => Jetstream::hasTermsAndPrivacyPolicyFeature(),
         ]);
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * @dataProvider validationDataProvider
+     */
+    public function test_has_proper_validation_rules(string $fieldKey, array $inputData): void
+    {
+        $controlUser = User::factory()->create(
+            [
+                'email' => 'test@test.com',
+                'username' => 'test',
+            ]
+        );
+
+        $dummyUser = User::factory()->make();
+
+
+        $response = $this->post('/register', [
+            'name'                  => $dummyUser->name,
+            'username'              => $dummyUser->username,
+            'email'                 => $dummyUser->email,
+            'password'              => 'password',
+            'password_confirmation' => 'password',
+            'terms'                 => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+            ...$inputData
+        ])->assertSessionHasErrors([$fieldKey]);
+
+    }
+
+    public static function validationDataProvider(): array
+    {
+        return [
+            'Username is required' => ['username', ['username' => null]],
+            'Username must have a minimum length of 1' => ['username', ['username' => '']],
+            'Username must have a maximum length of 50' => ['username', ['username' => (new GenerateUsername)(Str::random(51))]],
+            'Username must follow a slug like format' => ['username', ['username' => 'this is not ok']],
+            'Username must be a string' => ['username', ['username' => ['test']]],
+            'Username is unique' => ['username', ['username' => 'test']],
+        ];
     }
 }
