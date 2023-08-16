eb2c33e348cdd1145380ee57bad585183de20634
Brent Roose <brent.roose@gmail.com>
2023-08-16 13:32:49 +0200

    wip

--- database/migrations/2023_08_16_112210_create_user_argument_views_table.php
+++ database/migrations/2023_08_16_112210_create_user_argument_views_table.php
@@ -22,4 +22,9 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('user_argument_views');
+    }
 };
