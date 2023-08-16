aa1783dbddbdb73b6928b9333c11f5de27db46c9
SerhiiCho <serhiicho@protonmail.com>
2023-08-16 11:15:13 +0300

    Added successful validation messages to forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
             action="{{ route('password.email') }}"
             :heading="__('Reset password')"
         >
-            @csrf
+            <x-success-message />
 
             <p class="mb-5 text-gray-500">
                 {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
