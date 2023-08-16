a81b7c993ca3863c0870aa9bb2e9c0998ad315d3
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-16 21:39:10 +0300

    feat: add anchor tag

--- resources/views/components/argument-card/button.blade.php
+++ resources/views/components/argument-card/button.blade.php
@@ -1,5 +1,13 @@
-<button {{ $attributes->merge(['class' => 'px-5 py-1 text-[.9rem] rounded-full font-bold text-gray-900 transition-colors border']) }}>
-    <span class="flex items-center gap-2 justify-center">
-        <x-dynamic-component :component="$icon" class="w-4 h-4" /> {{ $slot }}
-    </span>
-</button>
+@if ($attributes->has('href'))
+    <a {{ $attributes->merge(['class' => 'px-5 py-1 text-[.9rem] rounded-full font-bold text-gray-900 transition-colors border']) }}>
+        <span class="flex items-center gap-2 justify-center">
+            <x-dynamic-component :component="$icon" class="w-4 h-4"/> {{ $slot }}
+        </span>
+    </a>
+@else
+    <button {{ $attributes->merge(['class' => 'px-5 py-1 text-[.9rem] rounded-full font-bold text-gray-900 transition-colors border']) }}>
+        <span class="flex items-center gap-2 justify-center">
+            <x-dynamic-component :component="$icon" class="w-4 h-4"/> {{ $slot }}
+        </span>
+    </button>
+@endif
