7431330c41f72df7525af02fddd1cb964b2e8c86
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-16 14:43:43 +0300

    Handle potential duplicate usernames in Socialite Callback
    Modified the user creation in SocialiteCallbackController to address potential issue of duplicate usernames. Introduced a condition to generate a new, unique username based on the socialite user's nickname if a username clash is detected. This ensures uniqueness of usernames even if the same nickname exists in the social platform.

--- app/Http/Controllers/SocialiteCallbackController.php
+++ app/Http/Controllers/SocialiteCallbackController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\GenerateUsername;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
 
 final readonly class SocialiteCallbackController
 {
@@ -15,10 +18,11 @@ final readonly class SocialiteCallbackController
         $user = User::query()->where('email', $socialiteUser->getEmail())->first();
 
         if (! $user) {
+
             $user = User::create([
                 'email' => $socialiteUser->getEmail(),
                 'name' => $socialiteUser->getName() ?? $socialiteUser->getEmail(),
-                'username' => $socialiteUser->getNickname(),
+                'username' => $this->resolveUserName($socialiteUser),
                 'reputation' => 0,
                 'socialite' => serialize($socialiteUser),
             ]);
@@ -27,5 +31,17 @@ final readonly class SocialiteCallbackController
         Auth::login($user);
 
         return redirect()->to('/');
+    }
+
+    private function resolveUserName(SocialiteUser $socialiteUser): string {
+        $username = (new GenerateUsername)($socialiteUser->getNickname()); // normalize username to our rules
+
+        /**
+         * We need to check for duplicates, this is because users can register directly on the app and because of
+         * that there is a chance that when a user attempts to register with github the username is already taken.
+         */
+        $usernameCount = User::query()->where('username', 'like', "{$username}%")->count() + 1;
+
+        return $usernameCount === 1 ? $username : "{$username}-{$usernameCount}";
     }
 }
