c6c23630cd77ec88fa7253fb906bf21d9e183d07
SerhiiCho <serhiicho@protonmail.com>
2023-08-16 11:20:55 +0300

    Changed mb-6 to mb-5 for success-message blade component

--- resources/views/components/success-message.blade.php
+++ resources/views/components/success-message.blade.php
@@ -1,5 +1,5 @@
 @if (session('status'))
-    <div class="mb-6 bg-blue-50 border border-blue-100 rounded-md p-4" role="alert">
+    <div class="mb-5 bg-blue-50 border border-blue-100 rounded-md p-4" role="alert">
         {{ session('status') }}
     </div>
 @endif
