327c4b6b2a4ed958bc58afe688c9258995595f01
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-16 21:48:13 +0300

    refactor: change button styling to enhance code reuse

--- resources/views/components/argument-card/button.blade.php
+++ resources/views/components/argument-card/button.blade.php
@@ -1,11 +1,18 @@
+@php
+    /**
+     * @var Illuminate\View\ComponentAttributeBag $attributes
+     */
+    $styles = 'px-5 py-1 text-[.9rem] rounded-full font-bold text-gray-900 transition-colors border';
+@endphp
+
 @if ($attributes->has('href'))
-    <a {{ $attributes->merge(['class' => 'px-5 py-1 text-[.9rem] rounded-full font-bold text-gray-900 transition-colors border']) }}>
+    <a {{ $attributes->merge(['class' => $styles]) }}>
         <span class="flex items-center gap-2 justify-center">
             <x-dynamic-component :component="$icon" class="w-4 h-4"/> {{ $slot }}
         </span>
     </a>
 @else
-    <button {{ $attributes->merge(['class' => 'px-5 py-1 text-[.9rem] rounded-full font-bold text-gray-900 transition-colors border']) }}>
+    <button {{ $attributes->merge(['class' => $styles]) }}>
         <span class="flex items-center gap-2 justify-center">
             <x-dynamic-component :component="$icon" class="w-4 h-4"/> {{ $slot }}
         </span>
