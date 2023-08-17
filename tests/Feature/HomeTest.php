0026c1334bfb8293c98daff44c8767aa0f69f016
SerhiiCho <serhiicho@protonmail.com>
2023-08-17 18:29:03 +0300

    Added 2 more tests to HomeTest.php

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,18 +2,35 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Tests\TestCase;
 
 class HomeTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use LazilyRefreshDatabase;
+
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $this->get('/')->assertStatus(200);
+    }
 
-        $response->assertStatus(200);
+    public function test_the_application_returns_a_successful_response_when_auth_user_visits_it(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->get('/')
+            ->assertStatus(200);
+    }
+
+    public function test_the_application_returns_a_successful_response_when_admin_visits_it(): void
+    {
+        $admin = User::factory()->create(['is_admin' => true]);
+
+        $this->actingAs($admin)
+            ->get('/')
+            ->assertStatus(200);
     }
 }
+
