430cb97d14166ccab65cdc2dac08fd9d83d84f29
SerhiiCho <serhiicho@protonmail.com>
2023-08-17 09:51:49 +0300

    Added a new argument-card/card-footer blade component

---
+++ resources/views/components/argument-card/card-footer.blade.php
@@ -0,0 +1,22 @@
+@php
+    /**
+     * @var App\Models\Argument $argument
+     * @var App\Models\User|null $user
+     */
+@endphp
+
+<div class="flex gap-4 items-center justify-end my-4 mr-4 text-gray-500 opacity-90">
+    {{-- Label that shows when argument has been created --}}
+    <small>
+        {{ __('Created') }} {{ $argument->created_at->diffForHumans() }}
+    </small>
+
+    {{-- Label that shows if the argument has been seen --}}
+    @if ($user && $user->hasSeenArgument($argument))
+        <small class="flex gap-1.5 items-center">
+            <x-icons.eye class="w-4 h-4" />
+            <span>{{ __('Seen') }}</span>
+            <x-icons.check class="w-4 h-4" />
+        </small>
+    @endif
+</div>
