474a994cfc8fba541e1198866f8a0a0cc8df63ea
SerhiiCho <serhiicho@protonmail.com>
2023-08-17 16:17:50 +0300

    Added user and votes to card-footer

--- resources/views/components/argument-card/card-footer.blade.php
+++ resources/views/components/argument-card/card-footer.blade.php
@@ -6,21 +6,48 @@
      */
 @endphp
 
-<div class="flex flex-col lg:flex-row gap-4 items-center justify-between mb-3 mt-1 opacity-90">
+<div class="flex flex-col lg:flex-row gap-4 items-center justify-between mb-3 mt-1 opacity-80 border-t pt-3">
     <x-argument-card.share-links :argument="$argument" :anchor-link="$anchorLink" />
 
-    <div class="flex gap-4 items-center">
-        {{-- Label that shows when argument has been created --}}
-        <small>
-            {{ __('Created') }} {{ $argument->created_at->diffForHumans() }}
+    <div class="flex gap-3 items-center">
+        <small class="flex items-center gap-1">
+            @if ($user->getAvatarUrl())
+                <a
+                    href="{{ action(App\Http\Controllers\PublicProfileController::class, $user) }}"
+                    class="flex items-center gap-1.5 group"
+                >
+                    <img
+                        src="{{ $user->getAvatarUrl() }}"
+                        class="rounded-full shadow-sm w-[20px] h-[20px]"
+                        alt="{{ $user->username }} avatar"
+                    />
+
+                    <span class="group-hover:underline">{{ $user->username }}</span>
+                </a>
+            @endif
+
+            {{ __('voted') }}
+
+            <b @class([
+                'tracking-wide uppercase',
+                'text-green-700' => $argument->vote_type->isYes(),
+                'text-red-700' => $argument->vote_type->isNo(),
+            ])>
+                {{ $argument->vote_type->value }}
+            </b>
         </small>
 
-        {{-- Label that shows if the argument has been seen --}}
-        @if ($user && $user->hasSeenArgument($argument))
+        <span>•</span>
+
+        {{-- Label that shows when argument has been created --}}
+        <small>{{ $argument->created_at->diffForHumans() }}</small>
+
+        {{-- Label that shows if the argument haven't been seen --}}
+        @if ($user && !$user->hasSeenArgument($argument))
+            <span>•</span>
             <small class="flex gap-1.5 items-center">
-                <x-icons.eye class="w-4 h-4" />
-                <span>{{ __('Seen') }}</span>
-                <x-icons.check class="w-4 h-4" />
+                <x-icons.check-badge class="w-4 h-4" />
+                <span>{{ __('New') }}</span>
             </small>
         @endif
     </div>
