61817ae464f83c18539df09a71c4a6ed572f8c41
SerhiiCho <serhiicho@protonmail.com>
2023-08-17 10:44:11 +0300

    Small code styles fixes on button.blade.php

--- resources/views/components/argument-card/button.blade.php
+++ resources/views/components/argument-card/button.blade.php
@@ -1,20 +1,22 @@
 @php
     /**
      * @var Illuminate\View\ComponentAttributeBag $attributes
+     * @var string $icon
      */
+
     $styles = 'px-5 py-1 text-[.9rem] rounded-full font-bold text-gray-900 transition-colors border';
 @endphp
 
 @if ($attributes->has('href'))
     <a {{ $attributes->merge(['class' => $styles]) }}>
         <span class="flex items-center gap-2 justify-center">
-            <x-dynamic-component :component="$icon" class="w-4 h-4"/> {{ $slot }}
+            <x-dynamic-component :component="$icon" class="w-4 h-4" /> {{ $slot }}
         </span>
     </a>
 @else
     <button {{ $attributes->merge(['class' => $styles]) }}>
         <span class="flex items-center gap-2 justify-center">
-            <x-dynamic-component :component="$icon" class="w-4 h-4"/> {{ $slot }}
+            <x-dynamic-component :component="$icon" class="w-4 h-4" /> {{ $slot }}
         </span>
     </button>
 @endif
