0ac764761d83289043ed20a97ca41ee575dbdf19
SerhiiCho <serhiicho@protonmail.com>
2023-08-17 11:23:33 +0300

    Added appending random number to username in UserFactory
    I was causing tests to fail because sometimes user will be created with the username that is already in the database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
 
         return [
             'name' => $name,
-            'username' => (new GenerateUsername)($name),
+            'username' => (new GenerateUsername)($name) . '-' . random_int(1, 9999),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
