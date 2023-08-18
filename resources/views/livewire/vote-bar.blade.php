bcd1505141a638b7b25d3b140250d4863937eb8f
SerhiiCho <serhiicho@protonmail.com>
2023-08-18 12:09:04 +0300

    Refactored the structure of vote-bar.blade.php

--- resources/views/livewire/vote-bar.blade.php
+++ resources/views/livewire/vote-bar.blade.php
@@ -10,34 +10,27 @@
             @class([
                 'p-4 flex-grow text-left md:min-w-[15%] min-w-[20%]',
                 'hover:bg-green-600 hover:text-white cursor-pointer' => ! $hasVoted,
-                $voteType === App\Models\VoteType::YES ? 'bg-green-600 text-white' : 'bg-green-300 text-green-900',
+                'bg-green-600 text-white' => $voteType === App\Models\VoteType::YES,
+                'bg-green-300 text-green-900' => $voteType !== App\Models\VoteType::YES,
             ])
             style="width: {{ $rfc->percentage_yes }}%;"
 
             @if(! $hasVoted)
-                wire:click="vote('{{ \App\Models\VoteType::YES }}')"
+                wire:click="vote('{{ App\Models\VoteType::YES }}')"
             @endif
         >
             {{ $rfc->percentage_yes }}%
         </div>
 
         <div
-            class="
-                    p-4 flex-grow text-right
-                    md:min-w-[15%]
-                    min-w-[20%]
-
-                    @if(! $hasVoted)
-                    hover:bg-red-600 hover:text-white cursor-pointer
-                    @endIf
-
-                    @if($voteType === \App\Models\VoteType::NO)
-                    bg-red-600 text-white
-                    @else
-                    bg-red-300 text-red-900
-                    @endif
-                "
+            @class([
+                'p-4 flex-grow text-right md:min-w-[15%]min-w-[20%]',
+                'hover:bg-red-600 hover:text-white cursor-pointer' => ! $hasVoted,
+                'bg-red-600 text-white' => $voteType === App\Models\VoteType::NO,
+                'bg-red-300 text-red-900' => $voteType !== App\Models\VoteType::NO,
+            ])
             style="width: {{ $rfc->percentage_no }}%;"
+
             @if(! $hasVoted)
                 wire:click="vote('{{ \App\Models\VoteType::NO }}')"
             @endif >
@@ -49,8 +42,8 @@
         <div class="flex justify-center mt-6 font-bold items-baseline gap-1">
             {{ $userArgument ? "You've voted" : "You're voting" }}&nbsp;<span @class([
                 'p-1 px-3 rounded-full text-white shadow-md',
-                'bg-green-500' => $voteType === \App\Models\VoteType::YES,
-                'bg-red-500' => $voteType === \App\Models\VoteType::NO,
+                'bg-green-500' => $voteType === App\Models\VoteType::YES,
+                'bg-red-500' => $voteType === App\Models\VoteType::NO,
             ])>{{ $voteType->value }}</span>@if(!$userArgument)
                 Next, give your arguments:
             @else
@@ -61,41 +54,27 @@
 
     @if(!$userArgument && $voteType)
         <div class="flex {{ $voteType->getJustify() }} mt-6">
-            <div class="
-                flex-1
-                p-4 flex gap-4
-                items-end
-                {{ $voteType->getJustify() }}
-                bg-white
-            border-gray-200
-            @if ($voteType === \App\Models\VoteType::YES)
-                border-l-green-400
-                border-l-8
-                md:mr-8
-            @else
-                border-r-red-400
-                border-r-8
-                md:ml-8
-            @endif
-            shadow-md
-            p-4 gap-4 items-center
-        ">
+            <div @class([
+                'flex-1 p-4 flex gap-4 items-end bg-white border-gray-200 shadow-md p-4 gap-4 items-center',
+                $voteType->getJustify(),
+                'border-l-green-400 border-l-8 md:mr-8' => $voteType === App\Models\VoteType::YES,
+                'border-r-red-400 border-r-8 md:ml-8' => $voteType !== App\Models\VoteType::YES,
+            ])>
                 <div class="w-full">
-                    <small>
-                        Your argument:
-                    </small>
+                    <small>Your argument:</small>
 
                     <div class="grid gap-2">
                         <x-markdown-editor
                             wire:model="body"
-                            class="
-                                w-full border border-{{ $voteType->getColor() }}-200
-                                active:border-{{ $voteType->getColor() }}-400
-                                rounded
-                               "
+                            @class([
+                                'rounded w-full border',
+                                'border-green-200 active:border-green-200' => $voteType->getColor() === 'green',
+                                'border-red-200 active:border-red-200' => $voteType->getColor() === 'red',
+                           ])
                         />
+
                         @error('body')
-                        {{ $message }}
+                            {{ $message }}
                         @enderror
                     </div>
                 </div>
@@ -103,32 +82,26 @@
                 <div class="flex flex-col gap-2">
                     <button
                         type="submit"
-                        class="
-                            font-bold
-                            class='
-                            @if(empty($this->body))
-                             cursor-not-allowed
-                            @else
-                            cursor-pointer
-                            hover:bg-{{ $voteType->getColor() }}-600
-                            @endif
-                            py-2 px-4
-                            bg-{{ $voteType->getColor() }}-400
-                            text-white
-                            text-center
-                            rounded-full
-                        "
+                        @class([
+                            'font-bold py-2 px-4 text-white text-center rounded-full',
+                            'cursor-not-allowed' => empty($this->body),
+                            'cursor-pointer hover:bg-green-600' => ! empty($this->body) && $voteType->getColor() === 'green',
+                            'cursor-pointer hover:bg-red-600' => ! empty($this->body) && $voteType->getColor() === 'red',
+                            'bg-green-400' => $voteType->getColor() === 'green',
+                            'bg-red-400' => $voteType->getColor() === 'red',
+
+                        ])
                         wire:click="storeArgument"
-                    >Submit
+                    >
+                        Submit
                     </button>
 
-                    <button class="
-                        bg-gray-100
-                        hover:bg-gray-200
-                        py-2 px-4
-                        text-center
-                        rounded-full
-                    " wire:click="cancel">Cancel</button>
+                    <button
+                        class="bg-gray-100 hover:bg-gray-200 py-2 px-4 text-center rounded-full"
+                        wire:click="cancel"
+                    >
+                        Cancel
+                    </button>
                 </div>
             </div>
         </div>
