459e2aa2fc56aad4d1aa85353c8b53fedc3180d0
SerhiiCho <serhiicho@protonmail.com>
2023-08-18 21:25:05 +0300

    Reduced vertical padding for buttons/main component

--- resources/views/components/buttons/main.blade.php
+++ resources/views/components/buttons/main.blade.php
@@ -4,7 +4,7 @@
      * @var string $slot
      */
 
-    $styles = 'bg-purple-800 hover:bg-purple-900 inline-flex gap-3 px-4 py-2.5 px-4 transition-colors rounded-md font-bold text-white';
+    $styles = 'bg-purple-800 hover:bg-purple-900 inline-flex gap-3 px-4 py-2 px-4 transition-colors rounded-md font-bold text-white';
 @endphp
 
 @if ($attributes->has('href'))
