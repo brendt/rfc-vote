92b059e87bace5aa9cdafb19b9fc8dd6833db9cc
SerhiiCho <serhiicho@protonmail.com>
2023-08-19 10:58:30 +0300

    Added buttons/main-small button

--- resources/views/components/buttons/main-small.blade.php
+++ resources/views/components/buttons/main-small.blade.php
@@ -4,7 +4,7 @@
      * @var string $slot
      */
 
-    $styles = 'bg-purple-800 hover:bg-purple-900 inline-flex items-center gap-3 py-2 px-4 transition-colors rounded-md font-bold text-white cursor-pointer';
+    $styles = 'bg-purple-800 hover:bg-purple-900 inline-flex items-center justify-center gap-3 px-3 py-1 transition-colors rounded-md font-bold text-white text-sm cursor-pointer';
 @endphp
 
 @if ($attributes->has('href'))
