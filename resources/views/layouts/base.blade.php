f53be29eaf3b8220c9bdc4894b04b411ac2b5d47
Mohammad_Alhallaq <mohammad.alhallaq@gmail.com>
2023-08-20 15:19:51 +0300

    Replace vanilla JS with Alpine.js to handle the state of the 'To Top' button

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -88,13 +88,26 @@
 </div>
 
 @if(isset($showToTopArrow) && $showToTopArrow === true)
-    <div id="scrollTopButton" class="sticky hidden w-full justify-end bottom-0 right-0 pb-3 pr-5">
-        <button onclick="scrollToTop()"
-                class="transition rounded-full bg-purple-600 p-4 text-white shadow-md hover:bg-purple-700 duration-700 ease-in-out hover:-translate-y-3 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg active:bg-purple-800 active:shadow-lg">
-            <x-dynamic-component
-                :component="'icons.arrow-double-up'"
-            />
-        </button>
+    <div class="sticky flex w-full justify-end bottom-0 right-0 pb-3 pr-5"
+         x-init="window.addEventListener('scroll', () => { scrolled = window.scrollY; updateVisibility() })"
+         x-data="{ isVisible: false, scrolled: 0, updateVisibility() { this.isVisible = (this.scrolled / (document.documentElement.scrollHeight - window.innerHeight)) >= 0.7; } }"
+         x-show="isVisible"
+    >
+        <div x-show="isVisible"
+             x-transition:enter="transition ease-out duration-300"
+             x-transition:enter-start="opacity-0 transform translate-y-2"
+             x-transition:enter-end="opacity-100 transform translate-y-0"
+
+             x-transition:leave="transition ease-in duration-300"
+             x-transition:leave-start="opacity-100 transform"
+             x-transition:leave-end="opacity-0 transform"
+        >
+            <button onclick="window.scrollTo({top: 0, behavior: 'smooth'});"
+                    class="rounded-full bg-purple-600 p-4 text-white shadow-md hover:bg-purple-700 duration-700 hover:-translate-y-3 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg active:bg-purple-800 active:shadow-lg"
+            >
+                <x-icons.arrow-double-up/>
+            </button>
+        </div>
     </div>
 @endif
 
@@ -124,24 +137,5 @@
 </div>
 
 @livewireScripts
-
-
-<script>
-    const scrollFunction = () => {
-        const myButton = document.getElementById("scrollTopButton");
-        const scrollThreshold = (document.documentElement.scrollHeight - window.innerHeight) * (60 / 100);
-        if (document.documentElement.scrollTop > scrollThreshold) {
-            myButton.classList.remove("hidden");
-            myButton.classList.add("flex");
-        } else {
-            myButton.classList.remove("flex");
-            myButton.classList.add("hidden");
-        }
-    };
-    window.addEventListener("scroll", scrollFunction);
-    const scrollToTop = () => {
-        window.scrollTo({top: 0});
-    }
-</script>
 </body>
 </html>
