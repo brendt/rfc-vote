78fba1fdad5d7cd3d5594bc441194794880baad2
Mohammad_Alhallaq <mohammad.alhallaq@gmail.com>
2023-08-20 15:36:45 +0300

    Replace vanilla JS with Alpine.js to handle the state of the 'To Top' button

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -88,26 +88,25 @@
 </div>
 
 @if(isset($showToTopArrow) && $showToTopArrow === true)
-    <div class="sticky flex w-full justify-end bottom-0 right-0 pb-3 pr-5"
+    <div class="sticky flex self-end justify-end bottom-6 right-6"
          x-init="window.addEventListener('scroll', () => { scrolled = window.scrollY; updateVisibility() })"
          x-data="{ isVisible: false, scrolled: 0, updateVisibility() { this.isVisible = (this.scrolled / (document.documentElement.scrollHeight - window.innerHeight)) >= 0.5; } }"
          x-show="isVisible"
     >
-        <div x-show="isVisible"
-             x-transition:enter="transition ease-out duration-300"
-             x-transition:enter-start="opacity-0 transform translate-y-2"
-             x-transition:enter-end="opacity-100 transform translate-y-0"
+        <button
+            x-show="isVisible"
+            x-transition:enter="transition ease-out duration-300"
+            x-transition:enter-start="opacity-0 transform translate-y-2"
+            x-transition:enter-end="opacity-100 transform translate-y-0"
 
-             x-transition:leave="transition ease-in duration-300"
-             x-transition:leave-start="opacity-100 transform"
-             x-transition:leave-end="opacity-0 transform"
+            x-transition:leave="transition ease-in duration-300"
+            x-transition:leave-start="opacity-100 transform"
+            x-transition:leave-end="opacity-0 transform"
+            onclick="window.scrollTo({top: 0});"
+            class="rounded-full bg-purple-600 p-4 text-white shadow-md hover:bg-purple-700 duration-700 hover:-translate-y-3 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg active:bg-purple-800 active:shadow-lg"
         >
-            <button onclick="window.scrollTo({top: 0});"
-                    class="rounded-full bg-purple-600 p-4 text-white shadow-md hover:bg-purple-700 duration-700 hover:-translate-y-3 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg active:bg-purple-800 active:shadow-lg"
-            >
-                <x-icons.arrow-double-up/>
-            </button>
-        </div>
+            <x-icons.arrow-double-up/>
+        </button>
     </div>
 @endif
 
