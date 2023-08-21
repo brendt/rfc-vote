65a5ce604ded1be28a1182d79a7ff8934db27862
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-22 00:17:25 +0300

    Improve Username validation and error handling
    Introduced a new Livewire component (UsernameInput) to handle username input and validation. It uses UsernameFormatRule to validate the username and provides an improved error message for user feedback. Also, modified the input component to receive dynamic content with slots for better reusability. This change enhances user experience by providing more specific feedback for invalid usernames, and also improves code maintainability by abstracting username validation logic into a reusable component.

---
+++ app/Http/Livewire/UsernameInput.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Rules\UsernameFormatRule;
+use Illuminate\Contracts\Foundation\Application as ApplicationAlias;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Livewire\Component;
+
+class UsernameInput extends Component
+{
+    public string $name;
+    public string $label;
+    public ?string $value;
+    public string $placeholder;
+    public bool $required;
+    public mixed $message;
+
+    protected function rules(): array
+    {
+        return [
+            'value' => ['required', 'string', 'min:1', 'max:50', 'unique:users,username', new UsernameFormatRule]
+        ];
+    }
+
+
+    public function render(): View|Application|Factory|ApplicationAlias
+    {
+        return view('livewire.username-input');
+    }
+
+    public function updated(string $propertyName):void
+    {
+        $this->validateOnly($propertyName);
+    }
+}
