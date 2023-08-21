96a1cc466c2f506abcfea5d800ef59225b4d94eb
SerhiiCho <serhiicho@protonmail.com>
2023-08-21 10:42:45 +0300

    Changed styles for rfc tags on rfc page

--- resources/views/livewire/rfc-counter.blade.php
+++ resources/views/livewire/rfc-counter.blade.php
@@ -1,4 +1,4 @@
-<x-tag class="{{ $voteType->class(yes: 'bg-agree', no: 'bg-disagree') }} text-white font-bold">
+<x-tag class="{{ $voteType->class(yes: 'bg-agree', no: 'bg-disagree') }} text-white font-bold uppercase">
     {{ $count }}
     {{ $voteType->value }}
 </x-tag>
