b8206395682a3a70ed5c451e64ecf267e5897828
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-22 00:52:52 +0300

    refactor: cleaning up, message is not required.

--- resources/views/livewire/username-input.blade.php
+++ resources/views/livewire/username-input.blade.php
@@ -3,8 +3,7 @@
     :label="$label"
     wire:model="value"
     :placeholder="$placeholder"
-    :message="$message"
-    required=""
+    :required="$required"
 >
     @error('value') <span class="text-sm text-red-600 leading-4 block mt-1 ml-1" role="alert">{{ $message }}</span> @enderror
 </x-form.input>
