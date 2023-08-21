a764d0f689ce5106c66925d79f59524f7fcdee70
SerhiiCho <serhiicho@protonmail.com>
2023-08-21 16:19:40 +0300

    Modified comment on main-small.blade.php button

--- resources/views/components/buttons/main-small.blade.php
+++ resources/views/components/buttons/main-small.blade.php
@@ -1,7 +1,7 @@
 @php
     /**
      * @var Illuminate\View\ComponentAttributeBag $attributes
-     * @var string $slot
+     * @var Illuminate\View\ComponentSlot $slot
      */
 
     $styles = 'bg-purple-800 hover:bg-purple-900 inline-flex items-center justify-center gap-3 px-3 py-1 transition-colors rounded-md font-bold text-white text-sm cursor-pointer';
