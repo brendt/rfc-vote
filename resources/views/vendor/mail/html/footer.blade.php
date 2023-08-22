4255c159e27819d566c3c2fb9833274b95972e50
SerhiiCho <serhiicho@protonmail.com>
2023-08-22 11:19:33 +0300

    Changed footer and header for markdown emails

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -3,6 +3,7 @@
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
 <td class="content-cell" align="center">
+<p>&copy; {{ date('Y') }} {{ config('app.name') }}.<br />This project is open source. <a href="https://github.com/brendt/rfc-vote" target="_blank">Contribute</a> and collaborate with us!</p>
 {{ Illuminate\Mail\Markdown::parse($slot) }}
 </td>
 </tr>
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,7 @@
-@props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+<a href="{{ config('app.url') }}">
+{{ config('app.name') }}
 </a>
 </td>
 </tr>
