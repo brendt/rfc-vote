ddf412d9cf69f67472cdfedf9238ae590c5d3af1
Mohammad_Alhallaq <mohammad.alhallaq@gmail.com>
2023-08-22 12:31:43 +0300

    exclude the current RFC from the additional rfcs to prevent duplication

--- app/Http/Controllers/RfcDetailController.php
+++ app/Http/Controllers/RfcDetailController.php
@@ -45,6 +45,7 @@ final readonly class RfcDetailController
         $additionalRfcs = Rfc::query()
             ->where('published_at', '<=', now()->startOfDay())
             ->where(fn (Builder $builder) => $builder->whereNull('ends_at')->orWhere('ends_at', '>', now()))
+            ->where('id', '!=', $rfc->id)
             ->when(filled($user), fn (Builder $builder) => $builder->whereDoesntHave('arguments', fn (Builder $builder) => $builder->where('user_id', $user->id)))
             ->with(['arguments', 'yesArguments', 'noArguments'])
             ->inRandomOrder()
