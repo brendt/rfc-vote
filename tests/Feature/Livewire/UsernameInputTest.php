845071ad4ae50c00ca5aefb669697f8c82238a44
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-22 09:25:51 +0300

    test: add some more test cases

--- tests/Feature/Livewire/UsernameInputTest.php
+++ tests/Feature/Livewire/UsernameInputTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature\Livewire;
 
+use App\Actions\GenerateUsername;
 use App\Http\Livewire\UsernameInput;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -17,37 +20,62 @@ class UsernameInputTest extends TestCase
 
     public function test_has_data_passed_correctly(): void
     {
-        $value = 'just-a-test';
+        $value       = 'just-a-test';
         $placeholder = 'placeholder';
-        $label = 'label';
-        $required = true;
+        $label       = 'label';
+        $required    = true;
 
         Livewire::test(
             UsernameInput::class,
             [
-                'value' => $value,
+                'value'       => $value,
                 'placeholder' => $placeholder,
-                'label' => $label,
-                'required' => $required,
+                'label'       => $label,
+                'required'    => $required,
             ]
         )
-            ->assertSet('value', $value)
-            ->assertSet('placeholder', $placeholder)
-            ->assertSet('label', $label)
-            ->assertSet('required', $required);
+                ->assertSet('value', $value)
+                ->assertSet('placeholder', $placeholder)
+                ->assertSet('label', $label)
+                ->assertSet('required', $required);
     }
 
-    public function test_value_is_required(): void
+    /**
+     * @dataProvider validationDataProvider
+     */
+    public function test_value_has_proper_validation(string $fieldKey, array $inputData): void
     {
+        User::factory()->create(
+            [
+                'email'    => 'test@test.com',
+                'username' => 'test',
+            ]
+        );
         Livewire::test(UsernameInput::class)
-            ->set('value', '')
-            ->assertHasErrors(['value' => 'required']);
+                ->set($fieldKey, $inputData[$fieldKey])
+                ->assertHasErrors([$fieldKey]);
     }
 
     public function test_value_follows_specific_rule(): void
     {
         Livewire::test(UsernameInput::class)
-            ->set('value', 'Another bad username')
-            ->assertSee('The value must be valid. Only lowercase ASCII characters are allowed. Hyphens can be used. Whitespace, underscores, and multiple hyphens are not permitted. The username cannot start or end with a hyphen.');
+                ->set('value', 'Another bad username')
+                ->assertSee(
+                    'The value must be valid. Only lowercase ASCII characters are allowed. Hyphens can be used. Whitespace, underscores, and multiple hyphens are not permitted. The username cannot start or end with a hyphen.'
+                );
     }
+
+    public static function validationDataProvider(): array
+    {
+        return [
+            'Value is required'                      => ['value', ['value' => null]],
+            'Value must have a minimum length of 1'  => ['value', ['value' => '']],
+            'Value must have a maximum length of 50' => [
+                'value',
+                ['value' => (new GenerateUsername)(Str::random(51))]
+            ],
+            'Value is unique'                        => ['value', ['value' => 'test']],
+        ];
+    }
+
 }
