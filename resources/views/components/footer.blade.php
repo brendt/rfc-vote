70dc206d393b79b1f1107b34a48ec6ed483a6bab
SerhiiCho <serhiicho@protonmail.com>
2023-08-22 20:48:32 +0300

    Small cleanup of the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,24 @@
 <div class="
     p-8 text-white flex justify-center md:gap-6 gap-4 flex-col bg-purple-800
-    bg-gradient-to-br from-purple-700 to-purple-900
+    bg-gradient-to-br from-main to-main-dark
 ">
     <div class="flex font-bold flex-wrap md:flex-nowrap gap-4 flex-1 justify-center">
-        <span class="w-full md:w-auto">This project is open source, you can help out.</span>
+        <span class="w-full md:w-auto">
+            {{ __('This project is open source, you can help out') }}
+        </span>
         <div class="w-full md:w-auto">
-            <a href="https://github.com/brendt/rfc-vote" class="bg-white px-4 p-2 text-purple-600 font-bold rounded hover:bg-purple-300 hover:text-purple-800">Check out the repository</a>
+            <a
+                href="https://github.com/brendt/rfc-vote"
+                class="bg-white px-3 py-2 text-main font-bold rounded hover:bg-purple-100 hover:text-main-dark"
+            >
+                {{ __('Check out the repository') }}
+            </a>
         </div>
     </div>
 
     <div class="md:text-center">
         <p>
-            Special thanks to <a class="text-purple-200 underline hover:no-underline" href="https://github.com/brendt/rfc-vote/graphs/contributors">numerous contributors</a> for sending PRs, and <a href="https://tighten.com/" class="text-purple-200 underline hover:no-underline">Tighten</a> for helping out with the design!
+            Special thanks to <a class="text-purple-200 underline hover:no-underline" href="https://github.com/brendt/rfc-vote/graphs/contributors">numerous contributors</a> for sending PRs and helping out!
         </p>
     </div>
 
