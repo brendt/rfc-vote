c135db24de9c5df279a73b6fc75bff8a63e43125
SerhiiCho <serhiicho@protonmail.com>
2023-08-22 20:40:22 +0300

    Moved footer into a separate component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,22 @@
+<div class="
+    p-8 text-white flex justify-center md:gap-6 gap-4 flex-col bg-purple-800
+    bg-gradient-to-br from-purple-700 to-purple-900
+">
+    <div class="flex font-bold flex-wrap md:flex-nowrap gap-4 flex-1 justify-center">
+        <span class="w-full md:w-auto">This project is open source, you can help out.</span>
+        <div class="w-full md:w-auto">
+            <a href="https://github.com/brendt/rfc-vote" class="bg-white px-4 p-2 text-purple-600 font-bold rounded hover:bg-purple-300 hover:text-purple-800">Check out the repository</a>
+        </div>
+    </div>
+
+    <div class="md:text-center">
+        <p>
+            Special thanks to <a class="text-purple-200 underline hover:no-underline" href="https://github.com/brendt/rfc-vote/graphs/contributors">numerous contributors</a> for sending PRs, and <a href="https://tighten.com/" class="text-purple-200 underline hover:no-underline">Tighten</a> for helping out with the design!
+        </p>
+    </div>
+
+    <div class="flex justify-center gap-2 text-sm">
+        <a href="/feed" class="underline hover:no-underline text-purple-200">RSS</a>
+        <a href="https://github.com/brendt/rfc-vote" class="underline hover:no-underline text-purple-200">GitHub</a>
+    </div>
+</div>
