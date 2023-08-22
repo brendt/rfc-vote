ae5dd1f7a56d5e32cdfdd83e3d691e7c638bfac3
vsergiu93 <vsergiu93@users.noreply.github.com>
2023-08-22 06:26:49 +0000

    Fix styling

--- tests/Feature/Livewire/UsernameInputTest.php
+++ tests/Feature/Livewire/UsernameInputTest.php
@@ -20,24 +20,24 @@ class UsernameInputTest extends TestCase
 
     public function test_has_data_passed_correctly(): void
     {
-        $value       = 'just-a-test';
+        $value = 'just-a-test';
         $placeholder = 'placeholder';
-        $label       = 'label';
-        $required    = true;
+        $label = 'label';
+        $required = true;
 
         Livewire::test(
             UsernameInput::class,
             [
-                'value'       => $value,
+                'value' => $value,
                 'placeholder' => $placeholder,
-                'label'       => $label,
-                'required'    => $required,
+                'label' => $label,
+                'required' => $required,
             ]
         )
-                ->assertSet('value', $value)
-                ->assertSet('placeholder', $placeholder)
-                ->assertSet('label', $label)
-                ->assertSet('required', $required);
+            ->assertSet('value', $value)
+            ->assertSet('placeholder', $placeholder)
+            ->assertSet('label', $label)
+            ->assertSet('required', $required);
     }
 
     /**
@@ -47,35 +47,34 @@ class UsernameInputTest extends TestCase
     {
         User::factory()->create(
             [
-                'email'    => 'test@test.com',
+                'email' => 'test@test.com',
                 'username' => 'test',
             ]
         );
         Livewire::test(UsernameInput::class)
-                ->set($fieldKey, $inputData[$fieldKey])
-                ->assertHasErrors([$fieldKey]);
+            ->set($fieldKey, $inputData[$fieldKey])
+            ->assertHasErrors([$fieldKey]);
     }
 
     public function test_value_follows_specific_rule(): void
     {
         Livewire::test(UsernameInput::class)
-                ->set('value', 'Another bad username')
-                ->assertSee(
-                    'The value must be valid. Only lowercase ASCII characters are allowed. Hyphens can be used. Whitespace, underscores, and multiple hyphens are not permitted. The username cannot start or end with a hyphen.'
-                );
+            ->set('value', 'Another bad username')
+            ->assertSee(
+                'The value must be valid. Only lowercase ASCII characters are allowed. Hyphens can be used. Whitespace, underscores, and multiple hyphens are not permitted. The username cannot start or end with a hyphen.'
+            );
     }
 
     public static function validationDataProvider(): array
     {
         return [
-            'Value is required'                      => ['value', ['value' => null]],
-            'Value must have a minimum length of 1'  => ['value', ['value' => '']],
+            'Value is required' => ['value', ['value' => null]],
+            'Value must have a minimum length of 1' => ['value', ['value' => '']],
             'Value must have a maximum length of 50' => [
                 'value',
-                ['value' => (new GenerateUsername)(Str::random(51))]
+                ['value' => (new GenerateUsername)(Str::random(51))],
             ],
-            'Value is unique'                        => ['value', ['value' => 'test']],
+            'Value is unique' => ['value', ['value' => 'test']],
         ];
     }
-
 }
