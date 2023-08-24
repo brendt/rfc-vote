c51d7fceb06dfce4e14eded9bd129986e047e4f3
Brent Roose <brent.roose@gmail.com>
2023-08-24 09:58:35 +0200

    wip

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,8 +1,20 @@
 @component('layouts.base')
-    <nav class="p-4 flex items-center gap-4">
-        <a href="{{ action(\App\Http\Controllers\RfcAdminController::class) }}">RFCs</a>
-        <a href="{{ action(\App\Http\Controllers\VerificationRequestsAdminController::class) }}">Verification Requests</a>
-    </nav>
+
+    <div class="bg-purple-100">
+        <div class="container mx-auto">
+            <nav class="py-3 px-6 md:px-2 flex items-center gap-8">
+                <x-navbar.link
+                    href="{{ action(\App\Http\Controllers\RfcAdminController::class) }}"
+                    :isActive="request()->is('admin/rfc')">RFCs
+                </x-navbar.link>
+
+                <x-navbar.link
+                    href="{{ action(\App\Http\Controllers\VerificationRequestsAdminController::class) }}"
+                    :isActive="request()->is('admin/verification-requests')">Verification Requests
+                </x-navbar.link>
+            </nav>
+        </div>
+    </div>
 
     {{ $slot }}
 @endcomponent
