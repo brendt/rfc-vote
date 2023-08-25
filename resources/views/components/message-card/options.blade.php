8741b0a624c3bc9c3874075eb694d29272693fca
SerhiiCho <serhiicho@protonmail.com>
2023-08-25 15:44:46 +0300

    Moved buttons to an ellipsis type menu

---
+++ resources/views/components/message-card/options.blade.php
@@ -0,0 +1,47 @@
+@php
+    /**
+     * @var App\Models\Message $message
+     */
+@endphp
+
+<div
+    class="absolute right-1 top-1"
+    x-data="{ open: true }"
+>
+    <button
+        type="button"
+        class="p-2 rounded-full group-hover:bg-gray-100"
+        @click="open = !open"
+        @click.away="open = false"
+    >
+        <x-icons.ellipsis-vertical class="w-7 h-7 text-gray-800" />
+    </button>
+
+    <div
+        x-cloak
+        x-show="open"
+        class="bg-white rounded-md border right-5 absolute divide-y w-[160px] shadow-md"
+    >
+        @if(! $message->isArchived())
+            @if($message->isUnread() || $message->isArchived())
+                <x-message-card.option wire:click="read({{ $message->id }})">
+                    <x-icons.check class="w-4 h-4"/> Mark as read
+                </x-message-card.option>
+            @endif
+
+            @if(! $message->isUnread())
+                <x-message-card.option wire:click="unread({{ $message->id }})">
+                    <x-icons.eye class="w-4 h-4"/> Mark as unread
+                </x-message-card.option>
+            @endif
+
+            <x-message-card.option wire:click="archive({{ $message->id }})">
+                <x-icons.archive class="w-4 h-4"/> Archive
+            </x-message-card.option>
+        @else
+            <x-message-card.option wire:click="unread({{ $message->id }})">
+                <x-icons.archive-remove class="w-4 h-4"/> Unarchive
+            </x-message-card.option>
+        @endif
+    </div>
+</div>
