e7b79507a045424b34fa69736509dac02be3a2ca
SerhiiCho <serhiicho@protonmail.com>
2023-08-25 15:25:36 +0300

    card.blade.php refactoring

--- resources/views/components/message-card/card.blade.php
+++ resources/views/components/message-card/card.blade.php
@@ -1,14 +1,16 @@
 @php
-/** @var \App\Models\Message $message */
+    /**
+     * @var App\Models\Message $message
+     */
 @endphp
 
-<div class="
-            border-2 border-transparent
-            {{ $message->isUnread() ? 'border-purple-200' : '' }}
-            bg-white rounded-xl shadow-md w-full group/card py-5 pl-4 pr-10 md:px-8 md:pt-7 flex gap-6 items-center relative
-        ">
+<div @class([
+    'border-2 border-transparent bg-white rounded-xl shadow-md w-full',
+    'border-purple-200' => $message->isUnread(),
+    'group/card py-5 pl-4 pr-10 md:px-8 md:pt-7 flex gap-6 items-center relative',
+])>
     <div class="grid gap-2 md:gap-4 w-full">
-        {{ $message->body }}
+        <p>{{ $message->body }}</p>
 
         <div class="flex flex-col lg:flex-row gap-4 items-center justify-between mb-3 mt-1 opacity-80 border-t pt-3">
             <div class="text-xs">
