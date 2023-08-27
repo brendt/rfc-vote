b28c6bd7f34b5c59ffd04a0caabac46a404e9e7f
alirezasalehizadeh <alirezasalehizadeh@users.noreply.github.com>
2023-08-27 17:19:34 +0000

    Fix styling

--- app/Models/UserFlair.php
+++ app/Models/UserFlair.php
@@ -13,11 +13,11 @@ enum UserFlair: string
     public function getDescription(): string
     {
         return match ($this) {
-            UserFlair::CONTRIBUTOR => "This user has contribute to rfc-vote repository.",
-            UserFlair::INTERNALS => "This user is the PHP Internals people.",
-            UserFlair::ADMIN => "This user is Admin.",
-            UserFlair::LARAVEL => "This user is the member of Laravel core team.",
-            UserFlair::SYMFONY => "This user is the member of Symfony core team.",
+            UserFlair::CONTRIBUTOR => 'This user has contribute to rfc-vote repository.',
+            UserFlair::INTERNALS => 'This user is the PHP Internals people.',
+            UserFlair::ADMIN => 'This user is Admin.',
+            UserFlair::LARAVEL => 'This user is the member of Laravel core team.',
+            UserFlair::SYMFONY => 'This user is the member of Symfony core team.',
         };
     }
 }
