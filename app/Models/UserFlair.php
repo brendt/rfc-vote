efee063b60e274bd8442454a1f10f54a64664c90
Alireza Salehizadeh <alirezasalehizadehco@gmail.com>
2023-08-27 16:10:56 +0330

    Add description for user flair

--- app/Models/UserFlair.php
+++ app/Models/UserFlair.php
@@ -9,4 +9,20 @@ enum UserFlair: string
     case ADMIN = 'admin';
     case LARAVEL = 'laravel';
     case SYMFONY = 'symfony';
+
+    public function getDescription(): string
+    {
+        return $this->messages()[$this->value];
+    }
+
+    private function messages(): array
+    {   
+        return [
+            'contributor' => 'This user has contribute to rfc-vote repository.',
+            'internals' => 'This user is the PHP Internals people.',
+            'admin' => 'This user is Admin.',
+            'laravel' => 'This user is the member of Laravel core team.',
+            'symfony' => 'This user is the member of Symfony core team.',
+        ];
+    }
 }
