6c1eee47c1fdec4612a2fbc58bb39d08f4c32df6
SerhiiCho <serhiicho@protonmail.com>
2023-08-30 15:36:02 +0300

    Changed markdown to html on about me page #206

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,49 +1,52 @@
 @component('layouts.base')
-    <div class="container max-w-[600px] mx-auto prose prose-a:text-font-second prose-a:hover px-4 mb-10 text-font">
-<x-markdown>
-    ## <span class="text-font-second">Shaping PHP, together</span>
 
-<blockquote class="text-font-second">
-... voting is just the last step of the RFC process:
-   While the vote is the final arbiter, the discussion phase that precedes it
-   is where concerns are heard and the proposal is shaped.
-    <br><div style="text-align: right"><a href="https://externals.io/message/110936#110937" class="text-font-second">Nikita Popov</a></div>
-</blockquote>
+<div class="container mx-auto py-7 text-font prose prose-a:text-font hover:prose-a:text-font-second max-w-4xl">
+    <h2 class="text-font-second">Shaping PHP, together</h2>
+    <p>Thank you for checking out RFC Vote!</p>
+    <p>This project is dedicated to providing a platform for the PHP community to express their thoughts and feelings about the proposals for the PHP language in an easier way.</p>
+    <p>Our main goal is to visualize the diverse opinions and arguments surrounding PHP's proposed features, making it easier to understand the benefits and downsides of each proposal. By doing so, we hope to foster a greater understanding of how PHP developers feel about these changes.</p>
+    <p>While official voting on RFCs is limited to internal qualified developers and a specific group of contributors, RFC Vote offers a space for everyone in the PHP community to share their voice. Your votes and comments won't directly influence the official PHP RFC outcomes, but they can serve as valuable insights for those involved in the decision-making process.</p>
+    <p>In addition to casting a vote, you are encouraged to share your reasoning behind your choices on each RFC. By explaining why you voted yes or no, we can collectively gain better insights into the popularity or concerns associated with an RFC. This collaborative approach allows us to learn from one another and contributes to a more informed and connected PHP community.</p>
+    <p>For official messages, please consider using the PHP Internals mailing list. Despite common belief, it is in fact, open to anyone.</p>
 
-Thank you for checking out RFC Vote!
+    <blockquote class="text-font-second">
+        ... voting is just the last step of the RFC process:
+        While the vote is the final arbiter, the discussion phase that precedes it
+        is where concerns are heard and the proposal is shaped.
+        <br>
+        <div class="text-right">
+            <a href="https://externals.io/message/110936#110937" class="text-font-second">Nikita Popov</a>
+        </div>
+    </blockquote>
 
-This project is dedicated to providing a platform for the PHP community to express their thoughts and feelings about the proposals for the PHP language in an easier way.
 
-Our main goal is to visualize the diverse opinions and arguments surrounding PHP's proposed features, making it easier to understand the benefits and downsides of each proposal. By doing so, we hope to foster a greater understanding of how PHP developers feel about these changes.
+    <h2 class="text-font-second">Who's involved?</h2>
 
-While official voting on RFCs is limited to internal qualified developers and a specific group of contributors, RFC Vote offers a space for everyone in the PHP community to share their voice. Your votes and comments won't directly influence the official PHP RFC outcomes, but they can serve as valuable insights for those involved in the decision-making process.
+    <p>Many people throughout the PHP community have already <a href="https://github.com/brendt/rfc-vote/graphs/contributors" class="text-font-second">contributed to this project</a> — and <a href="https://github.com/brendt/rfc-vote" class="text-font-second">you can too</a>!</p>
+    <p>We also want to give a special shout-out to <a href="https://tighten.com/" class="text-font-second">Tighten</a> for helping to create a design.</p>
+    <p>The initial idea came from <a href="https://twitter.com/pronskiy" class="text-font-second">Roman Pronskiy</a>, who helps administer the <a href="https://thephp.foundation/" class="text-font-second">PHP Foundation</a>, and is developed (together with many open source enthusiasts) by <a href="https://twitter.com/brendt_gd" class="text-font-second">Brent Roose</a>, developer advocate for PHP at <a href="https://www.jetbrains.com/phpstorm/" class="text-font-second">JetBrains</a>.</p>
 
-In addition to casting a vote, you are encouraged to share your reasoning behind your choices on each RFC. By explaining why you voted yes or no, we can collectively gain better insights into the popularity or concerns associated with an RFC. This collaborative approach allows us to learn from one another and contributes to a more informed and connected PHP community.
+    <p>Thanks to all our contributors:</p>
 
-For official messages, please consider using the PHP Internals mailing list. Despite common belief, it is in fact, open to anyone.
+    <ul>
+        @foreach($contributors as $contributor)
+            <li>
+                <a href="{{ $contributor->url }}">{{ $contributor->name }}</a>:
+                <a href="{{ $contributor->contributionsUrl }}">{{ implode(', ', $contributor->contributions) }}</a>
+            </li>
+        @endforeach
+    </ul>
 
-## <span class="text-font-second">Who's involved?</span>
+    <h2 class="text-font-second">Interesting links</h2>
 
-Many people throughout the PHP community have already <a href="https://github.com/brendt/rfc-vote/graphs/contributors" class="text-font-second">contributed to this project</a> — and <a href="https://github.com/brendt/rfc-vote" class="text-font-second">you can too</a>!
-We also want to give a special shout-out to <a href="https://tighten.com/" class="text-font-second">Tighten</a> for helping to create a design.
-
-The initial idea came from <a href="https://twitter.com/pronskiy" class="text-font-second">Roman Pronskiy</a>, who helps administer the <a href="https://thephp.foundation/" class="text-font-second">PHP Foundation</a>, and is developed (together with many open source enthusiasts) by <a href="https://twitter.com/brendt_gd" class="text-font-second">Brent Roose</a>, developer advocate for PHP at <a href="https://www.jetbrains.com/phpstorm/" class="text-font-second">JetBrains</a>.
-
-Thanks to all our contributors:
-
-<ul>
-    @foreach($contributors as $contributor)
+    <ul>
         <li>
-            <a href="{{ $contributor->url }}">{{ $contributor->name }}</a>:
-            <a href="{{ $contributor->contributionsUrl }}">{{ implode(', ', $contributor->contributions) }}</a>
+            <a href="https://github.com/brendt/rfc-vote" class="text-font-second">The RFC Vote repository</a> — everyone can contribute!
         </li>
-    @endforeach
-</ul>
-
-## <span class="text-font-second">Interesting links</span>
-
-- <a href="https://github.com/brendt/rfc-vote" class="text-font-second">The RFC Vote repository</a> — everyone can contribute!
-- <a href="https://www.youtube.com/playlist?list=PL0bgkxUS9EaLguM2puiMD-NiiV6r5b8RY" class="text-font-second">The YouTube playlist</a> where we built this project on stream, together.
-</x-markdown>
+        <li>
+            <a href="https://www.youtube.com/playlist?list=PL0bgkxUS9EaLguM2puiMD-NiiV6r5b8RY" class="text-font-second">The YouTube playlist</a> where we built this project on stream, together.
+        </li>
+    </ul>
 </div>
+
 @endcomponent
