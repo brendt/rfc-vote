4fd35ea9936973204499d35228fd4aeda3381618
SerhiiCho <serhiicho@protonmail.com>
2023-08-30 22:03:17 +0300

    WIP on about me page

--- resources/views/components/about/contributors.blade.php
+++ resources/views/components/about/contributors.blade.php
@@ -8,15 +8,18 @@
 <x-about.section heading="Our contributors">
     <ul class="grid grid-cols-10 gap-4">
         @foreach($contributors as $c)
-            <li class="flex gap-2">
+            <li class="flex gap-2 rounded-l-full rounded-r-xl bg-background">
+                <img
+                    src="{{ "https://avatars.githubusercontent.com/u/{$c->id}" }}"
+                    alt="{{ $c->name }}"
+                    width="460"
+                    height="460"
+                    class="w-full shadow-md rounded-full"
+                />
+
                 <div>
-                    <img
-                        src="{{ "https://avatars.githubusercontent.com/u/{$c->id}" }}"
-                        alt="{{ $c->name }}"
-                        width="460"
-                        height="460"
-                        class="w-full shadow-md rounded-full"
-                    />
+                    <h2>{{ $c->name }}</h2>
+                    <p></p>
                 </div>
             </li>
         @endforeach
