efce48a3faa1cfbb1f2dbd192b8aa76075c72d57
Vytautas Smilingis <vytautas@whatagraph.com>
2023-08-30 17:17:48 +0200

    Testing `UpdateUserProfileInformation` action

--- tests/Feature/Profile/ProfileInformationTest.php
+++ tests/Feature/Profile/ProfileInformationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Profile;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
 use Laravel\Jetstream\Http\Livewire\UpdateProfileInformationForm;
 use Livewire\Livewire;
 use Tests\TestCase;
@@ -26,11 +27,29 @@ class ProfileInformationTest extends TestCase
     {
         $this->actingAs($user = User::factory()->create());
 
+        $file = UploadedFile::fake()->image('avatar.png');
+
         Livewire::test(UpdateProfileInformationForm::class)
             ->set('state', ['name' => 'Test Name', 'email' => 'test@example.com'])
+            ->set('photo', $file)
             ->call('updateProfileInformation');
 
         $this->assertEquals('Test Name', $user->fresh()->name);
         $this->assertEquals('test@example.com', $user->fresh()->email);
+        $this->assertNotNull($user->fresh()->profile_photo_path);
+    }
+
+    public function test_profile_update_force_full(): void
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $originalEmail = $user->email;
+
+        Livewire::test(UpdateProfileInformationForm::class)
+            ->set('state', ['name' => 'Test Name', 'email' => $originalEmail])
+            ->call('updateProfileInformation');
+
+        $this->assertEquals('Test Name', $user->fresh()->name);
+        $this->assertEquals($originalEmail, $user->fresh()->email);
     }
 }
