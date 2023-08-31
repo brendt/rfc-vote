d31a19b99ffa70eda5501627a0ba48a3ac1e81a8
SerhiiCho <serhiicho@protonmail.com>
2023-08-31 10:01:28 +0300

    Simplified AboutController.php

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -9,11 +9,11 @@ final readonly class AboutController
 {
     public function __invoke(): View
     {
-        $content = file_get_contents(__DIR__.'/../../../contributors.json');
-        $people = json_decode(file_get_contents(__DIR__.'/../../../contributors.json') ?: '{}', true);
+        $content = file_get_contents(__DIR__.'/../../../contributors.json') ?: '{}';
+        $people = json_decode($content, true, flags: JSON_THROW_ON_ERROR);
 
         $contributors = array_map(
-            fn (array $item) => new Contributor(...$item),
+            static fn (array $item) => new Contributor(...$item),
             $people['contributors'] ?? [],
         );
 
