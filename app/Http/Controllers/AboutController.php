b9f4ab32cc9dfe175efdbde764f5fa7be7e4e532
SerhiiCho <serhiicho@protonmail.com>
2023-08-31 11:21:25 +0300

    Added caching contributors

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,21 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Contributor;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Cache;
+use JsonException;
 
 final readonly class AboutController
 {
     public function __invoke(): View
     {
-        $content = file_get_contents(__DIR__.'/../../../contributors.json') ?: '{}';
-        $people = json_decode($content, true, flags: JSON_THROW_ON_ERROR);
-
-        $contributors = array_map(
-            static fn (array $item) => new Contributor(...$item),
-            $people['contributors'] ?? [],
-        );
+        $contributors = Cache::rememberForever('contributors', fn () => $this->getContributors());
 
         shuffle($contributors);
 
         return view('about', compact('contributors'));
+    }
+
+    /**
+     * @return Contributor[]
+     * @throws JsonException
+     */
+    private function getContributors(): array
+    {
+        $content = file_get_contents(__DIR__.'/../../../contributors.json') ?: '{}';
+        $people = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+
+        return array_map(
+            static fn (array $item) => new Contributor(...$item),
+            $people['contributors'] ?? [],
+        );
     }
 }
