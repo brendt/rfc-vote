206966dc8ed54a766298f127a8f3e793b59f7ea5
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-08-31 13:52:30 +0300

    test: start adding the login page object

---
+++ tests/Browser/Pages/LoginPage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser\Pages;
+
+use Laravel\Dusk\Browser;
+
+class LoginPage extends Page
+{
+    public function url(): string
+    {
+        return '/login';
+    }
+
+    public function assert(Browser $browser): void
+    {
+        $browser->assertPathIs($this->url());
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function elements(): array
+    {
+        return [
+            '@email-field' => 'input[name="email"]',
+            '@password-field' => 'input[name="password"]',
+            '@remember-me-field' => 'input[name="remember"]',
+        ];
+    }
+}
