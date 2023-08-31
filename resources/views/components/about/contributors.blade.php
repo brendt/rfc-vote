134a0469b6d66c714376f81f563705278be8bfac
SerhiiCho <serhiicho@protonmail.com>
2023-08-31 10:36:29 +0300

    Added styles to contributors

--- resources/views/components/about/contributors.blade.php
+++ resources/views/components/about/contributors.blade.php
@@ -6,27 +6,27 @@
 @endphp
 
 <x-about.section heading="Our contributors">
-    <ul class="flex flex-wrap justify-center gap-3">
+    <div class="flex flex-wrap justify-center gap-4">
         @foreach($contributors as $c)
-            <li @class([
-                'flex gap-4 rounded-[50px_13px_13px_50px] bg-background pl-1 pr-4 py-1 group',
-                'transition-all duration-300',
+            <a href="{{ $c->url }}" target="_blank" @class([
+                'flex gap-4 items-center transition-all duration-300 bg-background',
+                'rounded-[50px_13px_13px_50px] pr-4 hover:bg-admin-navbar-background',
             ])>
                 <img
                     src="{{ "https://avatars.githubusercontent.com/u/{$c->id}" }}"
                     alt="{{ $c->name }}"
                     width="460"
                     height="460"
-                    class="w-16 h-auto shadow-md rounded-full"
+                    class="w-16 h-16 shadow-md rounded-full"
                 />
 
                 <div class="flex justify-center">
-                    <div class="flex-col justify-center text-[.9rem] leading-5 hidden group-hover:flex transition-all duration-300">
+                    <div class="flex-col justify-center text-[.9rem] leading-5 flex">
                         <span><b>Name:</b> {{ $c->name }}</span>
                         <span><b>Focus:</b> {{ implode(', ', $c->contributions) }}</span>
                     </div>
                 </div>
-            </li>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-about.section>
