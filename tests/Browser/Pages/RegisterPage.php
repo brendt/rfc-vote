472ba1476fffad341d3ce1cffc74c1e1bd7d6062
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-09-02 22:46:30 +0300

    test: add tests for register flow

---
+++ tests/Browser/Pages/RegisterPage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Browser\Pages;
+
+use Laravel\Dusk\Browser;
+
+class RegisterPage extends Page
+{
+    public function url(): string
+    {
+        return '/register';
+    }
+
+    public function assert(Browser $browser): void
+    {
+        $browser->assertPathIs($this->url());
+    }
+
+    public function fillRegisterFormAndSubmit(
+        Browser $browser = null,
+        string $name = null,
+        string $email = null,
+        string $username = null,
+        string $password = null,
+        string $confirmPassword = null
+    ): Browser {
+        $browser->type('name', $name)
+            ->type('email', $email)
+            ->type('username', $username)
+            ->type('password', $password)
+            ->type('password_confirmation', $confirmPassword)
+            ->press('Register');
+
+        return $browser;
+    }
+
+    public function elements(): array
+    {
+        return [
+            '@element' => '#selector',
+        ];
+    }
+}
