731a6282c28aa6c6661ee9d52dac7138a9d937fe
brendt <brendt@users.noreply.github.com>
2023-09-04 12:00:14 +0000

    Fix styling

--- resources/views/components/argument-card/comment-form.blade.php
+++ resources/views/components/argument-card/comment-form.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-[50px_auto] gap-5">
     <div>
         <img
-            src="{{ $user->getAvatarUrl() }}"
+            src="{{ $user?->getAvatarUrl() ?? asset('default-avatar.jpg') }}"
             width="80"
             height="80"
             class="rounded-full shadow-md w-full mt-2"
@@ -21,11 +21,17 @@
                     'p-3 text-font bg-input ring-1 focus:ring-2 ring-transparent rounded-lg',
                     'focus:ring-2 ring-transparent focus:border-purple-300 focus:ring-purple-500/50 focus:outline-none',
                     'border border-divider w-full min-h-[110px]',
+                    'opacity-80 cursor-not-allowed' => !$user,
                 ])
-                placeholder="Write a comment ..."
+                placeholder="{{ $user ? 'Write a comment ...' : 'You need to be logged in to comment!' }}"
+                @disabled(!$user)
             ></textarea>
 
-            <x-buttons.main class="mt-2" type="submit">
+            <x-buttons.main
+                class="mt-2"
+                type="submit"
+                :disabled="!$user"
+            >
                 Comment
             </x-buttons.main>
         </form>
--- resources/views/components/buttons/main.blade.php
+++ resources/views/components/buttons/main.blade.php
@@ -4,7 +4,8 @@
      * @var Illuminate\View\ComponentSlot $slot
      */
 
-    $styles = 'bg-purple-800 hover:bg-purple-900 inline-flex items-center gap-3 py-2.5 px-6 transition-colors rounded-md font-bold text-white cursor-pointer';
+    $styles = 'bg-purple-800 inline-flex items-center gap-3 py-2.5 px-6 transition-colors rounded-md font-bold text-white ';
+    $styles .= $attributes->get('disabled') === true ? 'opacity-50 cursor-not-allowed' : 'hover:bg-purple-900 cursor-pointer';
 @endphp
 
 @if ($attributes->has('href'))
