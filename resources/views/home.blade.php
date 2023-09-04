9c476639b7d5896b0f4e7126c6e52a66c59c3df6
SerhiiCho <serhiicho@protonmail.com>
2023-09-04 13:00:55 +0300

    Fixed grid for the home cards on mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
             {{ __('Open RFCs') }}
         </x-home.title>
 
-        <div class="grid lg:grid-cols-3 gap-5">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
             @foreach ($rfcs as $rfc)
                 <x-card-link
                     :to="action(App\Http\Controllers\RfcDetailController::class, $rfc)"
