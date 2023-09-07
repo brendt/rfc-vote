629af398217fa3641b3b166a6235a0a09c62ca77
SerhiiCho <serhiicho@protonmail.com>
2023-09-07 20:17:27 +0300

    Formatted HTML in footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,23 +1,29 @@
-<div class="
-    p-8 text-white flex justify-center md:gap-6 gap-4 flex-col
-    bg-main-dark bg-gradient-to-r from-main to-main-dark">
+<div class="p-8 text-white flex justify-center md:gap-6 gap-4 flex-col bg-main-dark bg-gradient-to-r from-main to-main-dark">
     <div class="flex font-bold flex-wrap md:flex-nowrap gap-4 flex-1 justify-center">
         <span class="w-full md:w-auto">
-            {{ __('This project is open source, you can help out') }}
+            This project is open source, you can help out
         </span>
+
         <div class="w-full md:w-auto">
             <a
                 href="https://github.com/brendt/rfc-vote"
                 class="bg-white px-3 py-2 text-main font-bold rounded hover:bg-purple-100 hover:text-main-dark"
             >
-                {{ __('Check out the repository') }}
+                Check out the repository
             </a>
         </div>
     </div>
 
     <div class="md:text-center">
         <p>
-            Special thanks to <a class="text-purple-200 underline hover:no-underline" href="https://github.com/brendt/rfc-vote/graphs/contributors">numerous contributors</a> for sending PRs and helping out!
+            Special thanks to
+            <a
+                class="text-purple-200 underline hover:no-underline"
+                href="https://github.com/brendt/rfc-vote/graphs/contributors"
+            >
+                numerous contributors
+            </a>
+            for sending PRs and helping out!
         </p>
     </div>
 
