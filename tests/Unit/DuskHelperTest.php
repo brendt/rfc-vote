455037e77f4ca4ed592e0a3728a1a15514d436be
Sergiu Daniel Vidican <vsergiu93@gmail.com>
2023-09-07 19:41:03 +0300

    test: test new helper

---
+++ tests/Unit/DuskHelperTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use ArgumentCountError;
+use PHPUnit\Framework\TestCase;
+
+class DuskHelperTest extends TestCase
+{
+    public function test_it_renders_nothing_if_in_production_env(): void
+    {
+        app()->detectEnvironment(fn () => 'production');
+        $this->assertFalse(dusk('test'));
+    }
+
+    public function test_it_renders_dusk_selector_for_given_string(): void
+    {
+        $this->assertEquals('dusk="test"', dusk('test'));
+    }
+}
