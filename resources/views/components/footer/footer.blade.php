70c32312d16d691d429d07f5f32cd0e2f75fa217
SerhiiCho <serhiicho@protonmail.com>
2023-09-07 21:15:07 +0300

    Redesigned the footer

--- resources/views/components/footer/footer.blade.php
+++ resources/views/components/footer/footer.blade.php
@@ -1,34 +1,27 @@
-<div class="p-8 text-white flex justify-center md:gap-6 gap-4 flex-col bg-main-dark bg-gradient-to-r from-main to-main-dark">
-    <div class="flex font-bold flex-wrap md:flex-nowrap gap-4 flex-1 justify-center">
-        <span class="w-full md:w-auto">
-            This project is open source, you can help out
-        </span>
+<div class="py-16 text-white bg-main-dark bg-gradient-to-r from-main to-main-dark">
+    <div class="flex justify-center gap-8">
+        <x-footer.link href="/feed" icon="icons.rss">
+            RSS Feed
+        </x-footer.link>
 
-        <div class="w-full md:w-auto">
-            <a
-                href="https://github.com/brendt/rfc-vote"
-                class="bg-white px-3 py-2 text-main font-bold rounded hover:bg-purple-100 hover:text-main-dark"
-            >
-                Check out the repository
-            </a>
-        </div>
+        <x-footer.link href="https://github.com/brendt/rfc-vote" icon="icons.github">
+            Contribute
+        </x-footer.link>
+
+        <x-footer.link href="https://youtube.com/playlist?list=PL0bgkxUS9EaLguM2puiMD-NiiV6r5b8RY&si=IJHdypNSFGh5X-KJ" icon="icons.youtube">
+            Watch on YouTube
+        </x-footer.link>
+
+        <x-footer.link href="https://raw.githubusercontent.com/brendt/rfc-vote/main/LICENSE.md" icon="icons.document-text">
+            Our License
+        </x-footer.link>
     </div>
 
-    <div class="md:text-center">
-        <p>
-            Special thanks to
-            <a
-                class="text-purple-200 underline hover:no-underline"
-                href="https://github.com/brendt/rfc-vote/graphs/contributors"
-            >
-                numerous contributors
-            </a>
-            for sending PRs and helping out!
-        </p>
-    </div>
-
-    <div class="flex justify-center gap-2 text-sm">
-        <a href="/feed" class="underline hover:no-underline text-purple-200">RSS</a>
-        <a href="https://github.com/brendt/rfc-vote" class="underline hover:no-underline text-purple-200">GitHub</a>
+    <div class="text-center opacity-70 mt-5">
+        <small>
+            &copy; {{ date('Y') }} {{ config('app.name') }}. This project is open source.
+            <a href="https://github.com/brendt/rfc-vote" class="underline" target="_blank">Contribute</a>
+            and collaborate with us!</p>
+        </small>
     </div>
 </div>
