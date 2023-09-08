66d4eeb361078ecda78a2ad54baa753f03227387
SerhiiCho <serhiicho@protonmail.com>
2023-09-08 15:20:59 +0300

    Fixed styles on navbar link

--- resources/views/components/navbar/link.blade.php
+++ resources/views/components/navbar/link.blade.php
@@ -7,7 +7,7 @@
 
 <a
     {{ $attributes->merge([
-        'class' => 'relative group dark:text-gray-200 border-divider w-full md:w-auto p-4 md:p-1 md:border-none border-b last:border-none ' . ($isActive ? 'bg-background dark:bg-main md:bg-transparent' : '')
+        'class' => 'relative group text-gray-700 md:text-inherit dark:text-gray-300 text-[1rem] border-divider w-full md:w-auto p-4 md:p-1 md:border-none border-b last:border-none md:!bg-transparent ' . ($isActive ? 'bg-background dark:bg-main md:bg-transparent' : '')
     ]) }}
 >
     {{ $slot }}
@@ -15,7 +15,7 @@
     <div
         @class([
             'hidden md:block absolute bottom-0 top-7 -left-0.5 -right-0.5 transition-transform duration-300',
-            'group-hover:opacity-100 group-hover:translate-y-0 h-1 bg-purple-200 dark:bg-gray-500 rounded-full',
+            'group-hover:opacity-100 group-hover:translate-y-0 h-1 bg-purple-200 dark:bg-gray-400 rounded-full',
             $isActive ? 'opacity-0 md:opacity-100' : 'opacity-0 -translate-y-1',
         ])
     ></div>
