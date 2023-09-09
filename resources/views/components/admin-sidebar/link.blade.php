a4a528594eeffa81fc886b662894450f05d641fa
SerhiiCho <serhiicho@protonmail.com>
2023-09-09 12:35:30 +0300

    Fixed links on admin on mobile view

--- resources/views/components/admin-sidebar/link.blade.php
+++ resources/views/components/admin-sidebar/link.blade.php
@@ -8,13 +8,13 @@
 
 <a
     {{ $attributes->merge([
-        'class' => 'relative text-white px-4 w-64 py-3 flex gap-4 ' . ($isActive ? 'bg-purple-700' : 'hover:bg-black')
+        'class' => 'text-sm md:text-md relative text-white px-4 w-64 py-3 flex gap-4 ' . ($isActive ? 'bg-purple-700' : 'hover:bg-black')
     ]) }}
 >
     {{ $slot }}
 
     @isset($notificationNumber)
-        <span class="md:absolute ml-1 md:ml-0 top-1.5 left-8 bg-red-500 text-white text-xs rounded-full min-w-[17px] h-[17px] inline-flex items-center justify-center p-0.5">
+        <span class="absolute top-1.5 left-8 bg-red-500 text-white text-xs rounded-full min-w-[17px] h-[17px] inline-flex items-center justify-center p-0.5">
             {{ $notificationNumber }}
         </span>
     @endisset
