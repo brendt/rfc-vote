279f8b7da4bc64249f3371f0d10d92febb3c6ae6
SerhiiCho <serhiicho@protonmail.com>
2023-09-09 15:06:23 +0300

    Formatted app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,14 @@ import './bootstrap'
 import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
 import Clipboard from '@ryangjchandler/alpine-clipboard'
-import hljs from 'highlight.js/lib/core';
-import php from 'highlight.js/lib/languages/php';
+import hljs from 'highlight.js/lib/core'
+import php from 'highlight.js/lib/languages/php'
 
-hljs.registerLanguage('php', php);
+hljs.registerLanguage('php', php)
 hljs.highlightAll()
 Alpine.plugin(Clipboard)
 window.Alpine = Alpine
 
-Alpine.plugin( focus )
+Alpine.plugin(focus)
 
 Alpine.start()
