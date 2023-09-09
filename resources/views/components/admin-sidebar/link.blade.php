e02dd3b093fd2b5974a142ec25d95f551bdeada0
SerhiiCho <serhiicho@protonmail.com>
2023-09-09 12:32:44 +0300

    Added small red bubble notification to verification requests link

--- resources/views/components/admin-sidebar/link.blade.php
+++ resources/views/components/admin-sidebar/link.blade.php
@@ -1,14 +1,21 @@
 @php
     /**
      * @var boolean $isActive
+     * @var int|null $notificationNumber
      * @var Illuminate\View\ComponentAttributeBag $attributes
      */
 @endphp
 
 <a
     {{ $attributes->merge([
-        'class' => 'text-white px-4 w-72 py-3 flex gap-4 ' . ($isActive ? 'bg-purple-700' : 'hover:bg-black')
+        'class' => 'relative text-white px-4 w-64 py-3 flex gap-4 ' . ($isActive ? 'bg-purple-700' : 'hover:bg-black')
     ]) }}
 >
     {{ $slot }}
+
+    @isset($notificationNumber)
+        <span class="md:absolute ml-1 md:ml-0 top-1.5 left-8 bg-red-500 text-white text-xs rounded-full min-w-[17px] h-[17px] inline-flex items-center justify-center p-0.5">
+            {{ $notificationNumber }}
+        </span>
+    @endisset
 </a>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,9 +1,9 @@
 @component('layouts.base')
 
-<nav class="bg-gray-800 fixed inset-y-0 w-14 hover:w-72 flex flex-col opacity-80 hover:opacity-95 z-10 pt-16 divide-y divide-gray-600 overflow-hidden shadow-md transition-all">
+<nav class="bg-gray-800 fixed inset-y-0 w-14 hover:w-64 flex flex-col opacity-80 hover:opacity-95 z-10 pt-16 divide-y divide-gray-600 overflow-hidden shadow-md transition-all">
     <x-admin-sidebar.link
         href="{{ action(\App\Http\Controllers\RfcAdminController::class) }}"
-        :isActive="request()->is('admin/rfc')"
+        :is-active="request()->is('admin/rfc')"
     >
         <x-icons.rectangle-stack class="w-6 h-6" />
         RFCs
@@ -11,7 +11,7 @@
 
     <x-admin-sidebar.link
         href="{{ action(\App\Http\Livewire\UserList::class) }}"
-        :isActive="request()->segment(2) == 'users'"
+        :is-active="request()->segment(2) == 'users'"
     >
         <x-icons.users class="w-6 h-6" />
         Users
@@ -19,18 +19,15 @@
 
     <x-admin-sidebar.link
         href="{{ action(\App\Http\Controllers\VerificationRequestsAdminController::class) }}"
-        :isActive="request()->is('admin/verification-requests')"
+        :is-active="request()->is('admin/verification-requests')"
+        :notification-number="$pendingVerificationRequests"
     >
         <x-icons.check-circle class="w-6 h-6" />
         Verification Requests
-
-        @if($pendingVerificationRequests)
-            <span class="font-bold">({{ $pendingVerificationRequests }})</span>
-        @endif
     </x-admin-sidebar.link>
 </nav>
 
-<div class="pl-9 md:pl-14">
+<div class="pl-9 md:pl-12">
     {{ $slot }}
 </div>
 
