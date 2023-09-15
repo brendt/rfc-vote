9a1af999674624e6c1f7667a155f5c1d5e2ed2b7
SerhiiCho <serhiicho@protonmail.com>
2023-09-15 20:52:40 +0300

    Fixed github button on dark theme

--- resources/views/components/buttons/github.blade.php
+++ resources/views/components/buttons/github.blade.php
@@ -1,7 +1,7 @@
 <x-buttons.main
     href="{{ action(App\Http\Controllers\SocialiteRedirectController::class, 'github') }}"
-    class="!bg-gray-800 hover:!bg-gray-700"
+    class="!bg-gray-800 hover:!bg-gray-700 text-white dark:text-gray-300"
 >
-    <x-icons.github class="h-6 w-6 fill-white" />
+    <x-icons.github class="h-6 w-6 fill-white dark:fill-gray-300" />
     Log in with GitHub
 </x-buttons.main>
