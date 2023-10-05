fb0430a3251ec9baec66f850d0300582a837f795
Alireza Salehizadeh <alirezasalehizadehco@gmail.com>
2023-10-05 16:44:32 +0330

    Improve contributors section

--- resources/views/components/about/contributors.blade.php
+++ resources/views/components/about/contributors.blade.php
@@ -7,14 +7,14 @@
 
 <x-about.section heading="Our contributors">
     <div {{dusk('about-contributors')}} class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-2">
-        @foreach($contributors as $c)
-            <a href="{{ $c->url }}" target="_blank" @class([
+        @foreach($contributors as $contributor)
+            <a href="{{ $contributor->url }}" target="_blank" @class([
                 'flex gap-2 items-center transition-all duration-300 bg-background w-full',
                 'rounded-[50px_13px_13px_50px] pr-2 hover:bg-admin-navbar-background',
             ])>
                 <img
-                    src="{{ "https://avatars.githubusercontent.com/u/{$c->id}" }}"
-                    alt="{{ $c->name }}"
+                    src="{{ "https://avatars.githubusercontent.com/u/{$contributor->id}" }}"
+                    alt="{{ $contributor->name }}"
                     width="460"
                     height="460"
                     class="w-12 h-12 md:w-16 md:h-16 shadow-md rounded-full"
@@ -22,10 +22,10 @@
 
                 <div class="flex justify-center">
                     <div class="flex-col justify-center text-[.9rem] leading-5 flex my-0.5">
-                        <b class="{{ mb_strlen($c->name) > 10 ? 'text-xs' : 'text-sm' }}">{{ $c->name }}</b>
+                        <b class="{{ mb_strlen($contributor->name) > 10 ? 'text-xs' : 'text-sm' }}">{{ $contributor->name }}</b>
 
                         <span class="opacity-60 text-xs md:text-md leading-4">
-                            {{ implode(', ', $c->contributions) }}
+                            {{ implode(', ', $contributor->contributions) }}
                         </span>
                     </div>
                 </div>
