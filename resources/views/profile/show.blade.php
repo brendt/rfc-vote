436474eb917e874e76b354232b01f6a63c7654b7
SerhiiCho <serhiicho@protonmail.com>
2023-10-09 18:56:03 +0300

    Removed translation functions from some files

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            Profile
         </h2>
     </x-slot>
 
