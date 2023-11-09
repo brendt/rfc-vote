33a11c92b7002614e02fa603758b77d67d5dd14f
Brent Roose <brent.roose@gmail.com>
2023-11-09 13:38:28 +0100

    wip

--- tests/Unit/RfcSyncCommandTest.php
+++ tests/Unit/RfcSyncCommandTest.php
@@ -1,37 +1,30 @@
 <?php
 
-namespace Tests\Unit;
-
 use App\Console\Commands\RfcSyncCommand;
 use App\Support\ExternalsRssFeed;
 use Mockery\MockInterface;
-use SimpleXMLElement;
-use Tests\TestCase;
+use function Pest\Laravel\{artisan, assertDatabaseHas, assertDatabaseMissing};
 
-class RfcSyncCommandTest extends TestCase
-{
-    public function test_command(): void
-    {
-        $this->mock(ExternalsRssFeed::class, function (MockInterface $mock) {
-            $mock->shouldReceive('load')
-                ->once()
-                ->andReturn(new SimpleXMLElement(file_get_contents(base_path('tests/fixtures/externals.xml'))));
-        });
+test('command', function () {
+    $this->mock(ExternalsRssFeed::class, function (MockInterface $mock) {
+        $mock->shouldReceive('load')
+            ->once()
+            ->andReturn(new SimpleXMLElement(file_get_contents(base_path('tests/fixtures/externals.xml'))));
+    });
 
-        $this->artisan(RfcSyncCommand::class)->assertSuccessful();
+    artisan(RfcSyncCommand::class)->assertSuccessful();
 
-        $this->assertDatabaseHas('rfcs', [
-            'title' => '[VOTE] RFC Shorter attribute syntax change',
-            'url' => 'https://wiki.php.net/rfc/shorter_attribute_syntax_change',
-        ]);
+    assertDatabaseHas('rfcs', [
+        'title' => '[VOTE] RFC Shorter attribute syntax change',
+        'url' => 'https://wiki.php.net/rfc/shorter_attribute_syntax_change',
+    ]);
 
-        $this->assertDatabaseHas('rfcs', [
-            'title' => '[VOTE] RFC Without url',
-            'url' => null,
-        ]);
+    assertDatabaseHas('rfcs', [
+        'title' => '[VOTE] RFC Without url',
+        'url' => null,
+    ]);
 
-        $this->assertDatabaseMissing('rfcs', [
-            'title' => 'RFC Should not include this',
-        ]);
-    }
-}
+    assertDatabaseMissing('rfcs', [
+        'title' => 'RFC Should not include this',
+    ]);
+});
