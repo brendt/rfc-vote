81c278dc5b7fb9dce0bf97494d1e26874961a54d
Brent Roose <brent.roose@gmail.com>
2023-11-17 09:54:34 +0100

    Add notification feature in argument commenting
    This commit introduces a new feature in the 'CreateArgumentComment' action where each comment posted will notify the respective users. .

--- app/Actions/CreateArgumentComment.php
+++ app/Actions/CreateArgumentComment.php
@@ -19,5 +19,20 @@ final readonly class CreateArgumentComment
             'argument_id' => $argument->id,
             'body' => $body,
         ]);
+
+        $usersToNotify = $argument->comments
+            ->map(fn (ArgumentComment $comment) => $comment->user)
+            ->add($argument->user)
+            ->reject(fn (User $other) => $other->is($user))
+            ->unique(fn (User $user) => $user->id);
+
+        foreach ($usersToNotify as $userToNotify) {
+            (new SendUserMessage)(
+                to: $userToNotify,
+                sender: $user,
+                url: action(RfcDetailController::class, ['rfc' => $argument->rfc_id, 'argument' => $argument->id]),
+                body: 'wrote a new comment',
+            );
+        }
     }
 }
